6b77d3ba078fd03c33007aa7efc3e647b565a223
Danutzz8 <Marius.Prutean@outlook.com>
2021-02-05 22:39:28 +0200

    click is working

--- src/redux/app.saga.js
+++ src/redux/app.saga.js
@@ -1,11 +1,12 @@
-import { takeLatest, delay, put } from 'redux-saga/effects';
+import { takeEvery, takeLatest, delay, put } from 'redux-saga/effects';
 
 export function* onIncrement() {
   yield console.log('I am incremented');
-  yield delay(3000);
+  //yield delay(3000);
   yield put({ type: 'INCREMENT_FROM_SAGA' });
 }
 
 export function* incrementSaga() {
-  yield takeLatest('INCREMENT', onIncrement);
+  yield takeEvery('INCREMENT', onIncrement);
 }
+// takeLatest will trigger only one increment, if changed with takeEvery;
